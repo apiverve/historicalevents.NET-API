710bb0ba1a03c7799713a67b9e28c6b82c1e413a
APIVerve Team <hello@apiverve.com>
2024-08-20 10:54:26 -0500

    Bug Fixes

---
+++ HistoricalEventsQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class HistoricalEventsQueryOptions {
+public string text { get; set; }
+}
+
+}
